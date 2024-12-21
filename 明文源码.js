6f3b7c9a53caddaf95d866513de4bf17eeca5f7f
shakibaxo <136515149+shakibaxo@users.noreply.github.com>
2024-12-21 12:13:45 +0330

    Update 明文源码.js

--- 明文源码.js
+++ 明文源码.js
@@ -4,7 +4,7 @@ import { connect } from 'cloudflare:sockets';
 let userID = '';
 let proxyIP = '';
 let sub = '';
-let subConverter = 'SUBAPI.fxxk.dedyn.io';
+let subConverter = 'bpb.radically.pro';
 let subConfig = "https://raw.githubusercontent.com/ACL4SSR/ACL4SSR/master/Clash/config/ACL4SSR_Online_Mini_MultiMode.ini";
 let subProtocol = 'https';
 let subEmoji = 'true';
